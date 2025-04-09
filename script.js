846b31a39cfcc45a5bf73c55879f3b568aaff7b6
NerisClient <arturignatenko37@gmail.com>
2025-04-09 18:20:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const freeButton = document.querySelector(".free-button");
-
-    freeButton.addEventListener("click", () => {
-        alert("Ссылка на бесплатную версию уже доступна!");
-    });
-});
