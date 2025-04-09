6f1c453a4ed6b99af3270f58930a993a12752460
NerisClient <arturignatenko37@gmail.com>
2025-04-09 16:32:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const freeButton = document.querySelector(".free-button");
+
+    freeButton.addEventListener("click", () => {
+        alert("Ссылка на бесплатную версию будет добавлена позже!");
+    });
+});
