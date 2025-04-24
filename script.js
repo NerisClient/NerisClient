1a4cf25b4b030efee85bf457e173dadf2a891dab
NerisClient <arturignatenko37@gmail.com>
2025-04-24 18:27:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Функции для кнопок
+function buyPremium() {
+    window.location.href = 'https://funpay.com/users/11950500/'; // Укажите правильную ссылку для покупки
+  }
+  
+  function downloadFree() {
+    window.location.href = 'https://disk.yandex.ru/d/2zIdFmaAg_0YBA'; // Укажите правильную ссылку для скачивания
+  }
+  a
